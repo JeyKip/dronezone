5e56baa886c123dedb8230054a9ab2e0f7960f9c
JeyKip <genyanosolenko@mail.ru>
2017-10-23 17:37:03 +0300

    [new] Implemented basic markup for mobile devices

---
+++ dev/scripts/menu.js
@@ -0,0 +1,36 @@
+;(function(){
+    var changeMenuState = function(burger, menu, menuList) {
+        burger.classList.toggle('burger_opened');
+        menu.classList.toggle('menu_opened');
+
+        if (menu.classList.contains('menu_opened')) {
+            menu.style.maxHeight = menuList.clientHeight + 'px';
+        }
+        else {
+            menu.style.maxHeight = '0px';
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var container = document.getElementById('home');
+        if (container) {
+            var burger = container.getElementsByClassName('burger')[0];
+            var menu = container.getElementsByClassName('menu')[0];
+            var menuList = menu.getElementsByClassName('menu__items')[0];
+            if (burger && menu && menuList) {
+                burger.addEventListener('click', function(e) {
+                    changeMenuState(burger, menu, menuList);
+                });
+
+                var menuItems = menuList.getElementsByClassName('menu__item');
+                if (menuItems && menuItems.length) {
+                    for (var i = 0; i < menuItems.length; i++) {
+                        menuItems[i].addEventListener('click', function(){
+                            changeMenuState(burger, menu, menuList);
+                        });
+                    }
+                }
+            }
+        }
+    })
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const del = require('del');
 const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
@@ -24,10 +25,20 @@ gulp.task('less', function(){
                     relativeUrls: true
                 }))
                .pipe(autoprefixer())
-               .pipe(gulpIf(isDev, sourcemap.write()))
                .pipe(cssmin())
                .pipe(rename({suffix: '.min'}))
+               .pipe(gulpIf(isDev, sourcemap.write()))
                .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('scripts', function(){
+    return gulp.src('dev/scripts/*.js')
+               .pipe(gulpIf(isDev, sourcemap.init()))
+               .pipe(concat('script.js'))
+               .pipe(uglify())
+               .pipe(rename({suffix: '.min'}))
+               .pipe(gulpIf(isDev, sourcemap.write()))
+               .pipe(gulp.dest('build/scripts'));
 });
 
 gulp.task('assets', function(){
@@ -42,17 +53,22 @@ gulp.task('images', function(){
                .pipe(gulp.dest('build/styles/img'));
 });
 
+gulp.task('fonts', function(){
+    return gulp.src('dev/fonts/**/*.*', {since: gulp.lastRun('fonts')})
+               .pipe(gulp.dest('build/styles/fonts'));
+});
+
 gulp.task('clean', function(){
     return del('build');
 });
 
 gulp.task('build', gulp.series(
     'clean',
-    gulp.parallel('less', 'assets', 'images')));
+    gulp.parallel('less', 'scripts', 'assets', 'fonts', 'images')));
 
 gulp.task('watch', function(){
-    gulp.watch('dev/styles/**/*.less', gulp.series('less'))
-        .on('unlink', function(filePath){ deleteFileFromCache('less', filePath); });
+    gulp.watch('dev/styles/**/*.less', gulp.series('less'));
+    gulp.watch('dev/scripts/*.js', gulp.series('scripts'));
     gulp.watch('dev/assets/**/*.*', gulp.series('assets'))
         .on('unlink', function(filePath){ deleteFileFromCache('assets', filePath); });
 });
